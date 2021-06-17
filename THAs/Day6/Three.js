35f0fc27f479eb3db3240450e072ce09ed60efb4
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-06-17 07:05:15 +0530

    Temp Update

--- THAs/Day6/Three.js
+++ THAs/Day6/Three.js
@@ -1,6 +1,15 @@
 let test1 = [1, 2, 3, 4, 5, 6, 7];
-let test2 = ["Devsmest", "Baba", "ki", "Jao"];
+let test2 = [["apple", 2, 3], "Devsmest", "Baba", "ki", "Jao"];
 
 function first(val, length = 1) {
-    return val.slice(0, length);
+
+    let temp = val.slice(0, length);
+
+    // if(temp instanceof Object) {
+    //     temp.forEach(function(value) {
+    //         console.log(value);
+    //     });
+    // }
+
+    console.log("Array is: ", temp);
 }

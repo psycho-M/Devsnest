3db354cfbab7f43bf0e2b43e9a00e22a7c948e8a
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-06-29 20:57:54 +0530

    Day12 Update

--- THAs/DAy12/app.js
+++ THAs/DAy12/app.js
@@ -26,20 +26,49 @@ function addToDo() {
         }
         let input = todoInput.value;
         let newTodo = document.createElement("li");
-        let delBtn = document.createElement("button");
-        delBtn.classList.add("delete-btn", "hide");
-        delBtn.textContent = "DEL";
-        let checkBtn = document.createElement("button");
-        checkBtn.classList.add("done-btn", "hide");
-        checkBtn.textContent = "DONE";
+        // let delBtn = document.createElement("button");
+        // delBtn.classList.add("delete-btn", "hide");
+        // delBtn.textContent = "DEL";
+        // let checkBtn = document.createElement("button");
+        // checkBtn.classList.add("done-btn", "hide");
+        // checkBtn.textContent = "DONE";
         
         
     
         document.getElementById("todo-list").appendChild(newTodo);
         newTodo.id = "itm" + numberOfTasks++;
         newTodo.classList.add("itm");
-        newTodo.textContent = input;
-
+        // newTodo.textContent = input;
+        newTodo.innerHTML = '<button class="delete-btn hide">DEL</button><span class="list-input">' + input + '</span><button class="done-btn hide">CHECK</button>'
+        let buttons = newTodo.querySelectorAll('button');
+        console.log(buttons);
+        buttons.forEach(addFunctionality);
         todoInput.value = "";
+}
+
+function addFunctionality(btn) {
+    btn.addEventListener('click', addButtonFunction);
+}
+
+function addButtonFunction() {
+    if(this.classList.contains('delete-btn')) {
+        let currTodo = this.parentElement;
+        currTodo.remove();
+        return;
+    }
+
+    if(this.classList.contains('done-btn')) {
+        let currTodo = this.parentElement;
+        console.log(currTodo);
+        let listInput = currTodo.querySelector(".list-input").classList;
+        if(listInput.contains("checked")) {
+            listInput.remove("checked");
+            this.innerHTML = "CHECK";
+        } else {
+            listInput.add("checked");
+            this.innerHTML = "UNCHECK";
+        }
+        return;
+    }
 }
 

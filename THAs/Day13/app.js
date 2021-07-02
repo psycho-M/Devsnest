4f7d9c164430b25f3c9351020282a7a2a1c16d07
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-07-02 07:40:35 +0530

    Day13 Update

--- THAs/Day13/app.js
+++ THAs/Day13/app.js
@@ -23,9 +23,15 @@ function searchGif() {
 }
 
 function addGifs(input) {
-    let gifUrl = `https://api.giphy.com/v1/gifs/search?api_key=3ofpTs9JE0Uz2O6QssbXdD3YT2Q1uVSZ&q=` + input + `&limit=30&offset=0&rating=g&lang=en`;
+    let giphyGifUrl = `https://api.giphy.com/v1/gifs/search?api_key=3ofpTs9JE0Uz2O6QssbXdD3YT2Q1uVSZ&q=` + input + `&limit=30&offset=0&rating=g&lang=en`;
+    let tenorGifUrl = "https://g.tenor.com/v1/search?q=" + input + "&key=86KCD8X513JO&limit=30";
+    // fetchFromGiphy(giphyGifUrl);
+    fetchFromTenor(tenorGifUrl);
+}
 
-    fetch(gifUrl)
+
+function fetchFromGiphy(giphyGifUrl) {
+    fetch(giphyGifUrl)
         .then(response => response.json())
         .then(function (data) {
             let images = data.data;
@@ -40,11 +46,46 @@ function addGifs(input) {
                 console.log(currGifEl);
             }
             // console.log(data);
-            currGifNumber = 6;
+            // currGifNumber = 6;
+        })
+        .catch(() => {
+            alert("not found!");
         });
 }
 
-
-function addImage() {
-
+function fetchFromTenor(tenorGifUrl) {
+    fetch(tenorGifUrl)
+        .then(response => response.json())
+        .then(function (data) {
+            let gifs = data.results;
+            console.log(gifs);
+            for (let gif of gifs) {
+                let currGifUrl = gif.media[0].tinygif.url;
+                console.log(currGifUrl);
+                let currGifEl = document.createElement("img");
+                currGifEl.src = currGifUrl;
+                currGifEl.classList.add("gif");
+                resultSpace.appendChild(currGifEl);
+                console.log(currGifEl);
+            }
+            // console.log(data);
+            // currGifNumber = 6;
+        })
+        .catch(() => {
+            alert("not found!");
+        });
 }
+
+// let input = readLine();
+// let tenorGifUrl = "https://g.tenor.com/v1/search?q=" + input + "&key=86KCD8X513JO&limit=30";
+
+// fetch(tenorGifUrl)
+// .then(response => response.json())
+// .then(returned => {
+//     let gifs = returned.results;
+//     console.log("Success:", gifs);
+//     let gif1 = gifs[0];
+//     let gif1Url = gif1.media[0].tinygif.url;
+//     console.log(gif1Url);
+    
+// });

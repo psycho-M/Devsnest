08ed9685bad12201c5f59ecdaf90ddcf33c6de88
Mandeep Singh <61588507+psycho-M@users.noreply.github.com>
2021-07-07 20:23:29 +0530

    Day18 and Day16 Update

--- THAs/day16/src/index.js
+++ THAs/day16/src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './style.css';
 import ReactCard from './react_card';
-import ChessBoard from './chess_board';
 
 function MainPage(props) {
     return (
         <div className="container">
             <ReactCard />
-            <ChessBoard />
         </div>
     )
 }
---
+++ THAs/day18/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ChessBoard from './chess_board';
+
+function App() {
+    return (
+        <div className="App">
+            <ChessBoard />
+        </div>
+    );
+}
+
+export default App;
